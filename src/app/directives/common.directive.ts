e0dfc53269d12758e0940cb1ffa78f6a2392dac2
huabin.zhao <z85146549@163.com>
2016-08-09 16:16:24 +0800

    del

--- src/app/directives/common.directive.ts
+++
@@ -1,9 +0,0 @@
-import { Directive, HostListener} from '@angular/core';
-
-@Directive({ selector: '[noBubble]' })
-export class noBubble {
-    @HostListener('click', ['$event'])
-    onClick = (e: Event = window.event) => {
-        e.stopPropagation();
-    }
-}
